b92ea77245e008d78cdf1871eb684879d54619bd
Amandine Pallas <amandine.pallas@epsi.fr>
2012-12-10 20:43:12 +0100

    main & includes
    à changer maybe (test)

---
+++ FormCon.php
@@ -0,0 +1,15 @@
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+
+<div class="form">
+<form class="form-inline">
+  <input type="text" class="input-small" placeholder="Email">
+  <input type="password" class="input-small" placeholder="Password">
+  <label class="checkbox">
+    <input type="checkbox"> Se Souvenir
+  </label>
+  <button type="submit" class="btn">Se Connecter</button>
+</form>
+</div>
+<script src="http://code.jquery.com/jquery-latest.js"></script>
+<script src="js/bootstrap.min.js"></script>
---
+++ header.php
@@ -0,0 +1,31 @@
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+
+	
+<div class="media">
+  <a class="pull-right" href="# 	">
+    <img class="media-object" src="logo.jpg">
+  </a>
+  <div class="media-body">
+    <h4 class="media-heading"></h4>
+   
+ 
+    <!-- Nested media object -->
+    <div class="media">
+      
+    </div>
+  </div>
+</div>
+
+<div class="hero-unit">
+  <h1>PALASO</h1>
+  <p>Agence intérim spécialiste de l'informatique</p>
+  <p>
+    <a class="btn btn-primary btn-large">
+      En savoir plus
+    </a>
+  </p>
+</div>
+
+<script src="http://code.jquery.com/jquery-latest.js"></script>
+<script src="js/bootstrap.min.js"></script>
